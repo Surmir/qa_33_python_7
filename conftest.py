21897fc2e41d7f42f53e335a1a99793b083e6de1
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-13 18:29:16 +0300

    fixed fixture reg_and_del_courier

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
-from data import CourierData, OrderData
-from api import Courier, Orders
+from data import CourierData
+from api import Courier
 
 
 @pytest.fixture
@@ -14,7 +14,7 @@ def del_courier():
     Courier.delete(courier_id)
 
 @pytest.fixture
-def registr_and_del_courier():
+def reg_and_del_courier():
     Courier.create(CourierData.PAYLOAD_REGISTR)
     payload = CourierData.PAYLOAD_LOGIN
 
@@ -23,14 +23,3 @@ def registr_and_del_courier():
     response = Courier.login(payload)
     courier_id = response.json().get('id')
     Courier.delete(courier_id)
-
-@pytest.fixture
-def cancel_order():
-    payload = OrderData.PAYLOAD
-
-    yield payload
-
-    r = Orders.get_list_orders()
-    if r["orders"]["comment"] == payload["comment"]:
-        track = r["orders"]["track"]
-        Orders.cancel(track)
