7fa14ab3605fb619daf262566cf2286b197dd504
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-13 18:27:28 +0300

    fixed class CourierData

--- data.py
+++ data.py
@@ -1,23 +1,24 @@
 from urls import Url
+import helpers
 
 
 class CourierData():
 
     PAYLOAD_REGISTR = {
-        "login": 'hokage',
-        "password": '098123',
-        "firstName": 'naruto'
+        "login": helpers.generate_random_string(7),
+        "password": helpers.generate_random_string(7),
+        "firstName": helpers.generate_random_string(7)
     }
 
     PAYLOAD_REGISTR_SAME = {
-        "login": 'hokage',
-        "password": '345678',
-        "firstName": 'madara'
+        "login": PAYLOAD_REGISTR["login"],
+        "password": "345678",
+        "firstName": "madara"
     }
 
     PAYLOAD_LOGIN = {
-        "login": 'hokage',
-        "password": '098123'
+        "login": PAYLOAD_REGISTR["login"],
+        "password": PAYLOAD_REGISTR["password"]
     }
 
 class Api():
@@ -28,20 +29,17 @@ class Api():
     COURUER_CREATE = f"{url}/api/v1/courier"
     COURUER_DELETE = f"{url}/api/v1/courier/:id"
     ORDERS = f"{url}/api/v1/orders"
-    ORDERS_CANCEL = f"{url}/api/v1/orders/cancel"
 
 class OrderData():
 
     PAYLOAD = {
-    "firstName": "Madara",
-    "lastName": "Uchiha",
-    "address": "Konoha, 167 apt.",
-    "metroStation": 2,
-    "phone": "+7 800 366 56 56",
-    "rentTime": 4,
-    "deliveryDate": "2025-11-13",
-    "comment": "Saske, come back to Uchiha and rest",
-    "color": [
-        "GRAY"
-    ]
-}
+        "firstName": "Madara",
+        "lastName": "Uchiha",
+        "address": "Konoha, 167 apt.",
+        "metroStation": 5,
+        "phone": "+7 800 366 56 56",
+        "rentTime": 2,
+        "deliveryDate": "2025-11-13",
+        "comment": "Saske, come back to Uchiha and rest",
+        "color": ["GRAY"]
+    }
