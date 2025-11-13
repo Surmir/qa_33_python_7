e2eed1062e337c08331da42148e180ec6c9510f1
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-13 18:30:50 +0300

    added attr timeout

--- api.py
+++ api.py
@@ -6,19 +6,20 @@ import allure
 class Courier():
 
     @staticmethod
-    @allure.step("Запрос на получение логина курьера в системе")
+    @allure.step("Запрос на авторизацию курьера в системе")
     def login(data):
-        return requests.post(Api.COURUER_LOGIN, data=data)
+        return requests.post(Api.COURUER_LOGIN, json=data, timeout=5)
     
     @staticmethod
     @allure.step("Запрос на создание нового курьера")
     def create(data):
-        return requests.post(Api.COURUER_CREATE, data=data)
+        return requests.post(Api.COURUER_CREATE, json=data, timeout=10)
     
     @staticmethod
     @allure.step("Запрос на удаление курьера по id")
     def delete(id):
-        api_with_id = Api.COURUER_DELETE.replace(':id', id)
+        id_str = str(id)
+        api_with_id = Api.COURUER_DELETE.replace(':id', id_str)
         return requests.delete(api_with_id)
 
 class Orders():
@@ -31,9 +32,4 @@ class Orders():
     @staticmethod
     @allure.step("Запрос на создание заказа")
     def create(data):
-        return requests.post(Api.ORDERS, data=data)
-    
-    @staticmethod
-    @allure.step("Запрос на отмену заказа")
-    def cancel(track):
-        return requests.put(Api.ORDERS_CANCEL, data=track)
+        return requests.post(Api.ORDERS, json=data, timeout=5)
