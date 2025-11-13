76f967d584477814a957127be1fdc48761b4f5f8
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-13 18:48:59 +0300

    fix allure title

--- tests/test_api_orders.py
+++ tests/test_api_orders.py
@@ -6,7 +6,7 @@ from data import OrderData
 
 class TestApiOrders():
 
-    @allure.title("Проверка создания заказа")
+    @allure.title("Проверка создания заказа, с разными данными в color")
     @allure.description("Проверка создания заказа при заполнении поля цвет:" \
     " один цвет, два цвета, без цвета")
     @pytest.mark.parametrize('color', [["BLACK"], ["GREY"], ["BLACK", "GREY"], []])
