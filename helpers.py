eddcdd622b83cd53f8f0e60b71b7d52bda566c90
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-14 17:38:22 +0300

    moved class CourierData from data.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,27 @@
 import random
 import string
 
-def generate_random_string(length):
+
+def generate_random_string(self, length):
         letters = string.ascii_lowercase
         random_string = ''.join(random.choice(letters) for i in range(length))
         return random_string
+
+class CourierData():
+
+    PAYLOAD_REGISTR = {
+        "login": generate_random_string(7),
+        "password": generate_random_string(7),
+        "firstName": generate_random_string(7)
+    }
+
+    PAYLOAD_REGISTR_SAME = {
+        "login": PAYLOAD_REGISTR["login"],
+        "password": generate_random_string(7),
+        "firstName": generate_random_string(7)
+    }
+
+    PAYLOAD_LOGIN = {
+        "login": PAYLOAD_REGISTR["login"],
+        "password": PAYLOAD_REGISTR["password"]
+    }
