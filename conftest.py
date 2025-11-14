a5df19894b8ffd067e089240b733127539960089
Vasilii Kasatov <Vetquer@yandex.ru>
2025-11-14 18:25:16 +0300

    deleted fixture del_courier/change fixture reg_and_del_courier

--- conftest.py
+++ conftest.py
@@ -1,25 +1,15 @@
 import pytest
-from data import CourierData
-from api import Courier
+from helpers import CourierData
+from api import CourierApi
 
-
-@pytest.fixture
-def del_courier():
-    payload = CourierData.PAYLOAD_REGISTR
-
-    yield payload
-
-    response = Courier.login(CourierData.PAYLOAD_LOGIN)
-    courier_id = response.json().get('id')
-    Courier.delete(courier_id)
 
 @pytest.fixture
 def reg_and_del_courier():
-    Courier.create(CourierData.PAYLOAD_REGISTR)
-    payload = CourierData.PAYLOAD_LOGIN
-
+    payload = CourierData.PAYLOAD_REGISTR
+    CourierApi.create(payload)
+    
     yield payload
 
-    response = Courier.login(payload)
+    response = CourierApi.login(CourierData.PAYLOAD_LOGIN)
     courier_id = response.json().get('id')
-    Courier.delete(courier_id)
+    CourierApi.delete(courier_id)
